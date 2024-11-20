6de1f32fb748f66e11116467c0eef9d002f605ab
Semyon Savelyev <ctvbxm@mail.ru>
2024-11-20 20:14:10 +0300

    Динамические роутинги

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path("", main_view, name='main'),
+    path("animals/", animals_view, name='animals'),
+    path("animals/<str:animal>/", animal_view, name='animal'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,10 +1,27 @@
-from datetime import datetime
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-from django.shortcuts import render
 
-
-def main_view(request):
-    year = datetime.now().year
+def main_view(request, abracadabra):
+    sp = ['Олег', 'Андрей', 'Джон']
     return render(request, "web/index.html", {
-        "year": year
+        "sp": sp,
+        'number': 5
     })
+
+
+def animals_view(request):
+    return render(request, 'web/animals.html')
+
+
+def animal_view(request, animal):
+    age = request.GET.get('age')
+    animals = {
+        'bibizyana': 'Бибизяна прыгает',
+        'lion': 'Лев рычит'
+    }
+    if animal in animals:
+        return render(request, 'web/animal.html', {'animal_info': animals[animal], 'age': age})
+    else:
+        final_url = reverse('animal', args=('bibizyana', ))
+        return redirect(final_url)
